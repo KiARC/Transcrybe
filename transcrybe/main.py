4410ae619ecf483cde35dd1193dcf2acdf530755
Katherine Rose <kiarc@kiarc.win>
2024-09-11 00:55:04 +0000

    Store temp files in /tmp

--- transcrybe/main.py
+++ transcrybe/main.py
@@ -7,8 +7,8 @@ app = FastAPI()
 
 @app.post("/transcribe")
 def process_route(audio: UploadFile):
-    with open(audio.filename, "wb") as file:
+    with open(f"/tmp/{audio.filename}", "wb") as file:
         file.write(audio.file.read())
-    result = lib.transcribe(audio.filename)
-    os.remove(audio.filename)
+    result = lib.transcribe(f"/tmp/{audio.filename}")
+    os.remove(f"/tmp/{audio.filename}")
     return {"filename": audio.filename, "transcript_segments": [{"text": s["text"], "speaker": s["speaker"], "start": s["start"], "end": s["end"]} for s in result["segments"]]}
