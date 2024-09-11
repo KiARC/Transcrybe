6c8f4b3bc8c1ba975802948407cb38f73132690e
Katherine Rose <kiarc@kiarc.win>
2024-09-11 00:44:21 +0000

    Adjustments to endpoint function

--- transcrybe/main.py
+++ transcrybe/main.py
@@ -7,8 +7,8 @@ app = FastAPI()
 
 @app.post("/transcribe")
 def process_route(audio: UploadFile):
-    with open(f"to-process_{audio.filename}", "wb") as file:
+    with open(audio.filename, "wb") as file:
         file.write(audio.file.read())
-    result = lib.transcribe(f"to-process_{audio.filename}")
-    os.remove(f"to-process_{audio.filename}")
-    return [{"text": s["text"], "speaker": s["speaker"], "start": s["start"], "end": s["end"]} for s in result["segments"]]
+    result = lib.transcribe(audio.filename)
+    os.remove(audio.filename)
+    return {"filename": audio.filename, "transcript_segments": [{"text": s["text"], "speaker": s["speaker"], "start": s["start"], "end": s["end"]} for s in result["segments"]]}
