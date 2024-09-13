d7feab75aed3b293e6abab93c5e36f1b2ba6d45d
Katherine Rose <kiarc@kiarc.win>
2024-09-13 09:50:13 -0400

    Add an environment variable to configure model size and change default from small to base.en
    base.en should be more than twice as fast as small according to OpenAI's own testing, and if more accuracy is needed a larger, slower model can be configured per-deployment

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM python:3.12-bookworm
 
-ENV TRANSCRYBE_HF_TOKEN=PLEASE_ADD_A_TOKEN
+ENV TRANSCRYBE_HF_TOKEN=PLEASE_ADD_A_TOKEN \
+    TRANSCRYBE_MODEL_SIZE=base.en
 
 # Install poetry
 RUN curl -sSL https://install.python-poetry.org | python3 -
--- transcrybe/lib.py
+++ transcrybe/lib.py
@@ -4,23 +4,50 @@ import whisperx
 
 import whisperx.utils
 
-if not environ.get("TRANSCRYBE_HF_TOKEN") or environ.get("TRANSCRYBE_HF_TOKEN") == "PLEASE_ADD_A_TOKEN":
+if (
+    not environ.get("TRANSCRYBE_HF_TOKEN")
+    or environ.get("TRANSCRYBE_HF_TOKEN") == "PLEASE_ADD_A_TOKEN"
+):
     print("Please set the TRANSCRYBE_HF_TOKEN environment variable.")
     exit(1)
 
-trans_model = whisperx.load_model(
-    "small", "cpu", compute_type="int8", language="en")
+if not environ.get("TRANSCRYBE_MODEL_SIZE") or environ.get(
+    "TRANSCRYBE_MODEL_SIZE"
+) not in [
+    "tiny",
+    "tiny.en",
+    "base",
+    "base.en",
+    "small",
+    "small.en",
+    "medium",
+    "medium.en",
+    "large",
+    "large-v2",
+]:
+    print("Please set TRANSCRYBE_MODEL_SIZE a valid size for the whisper model.")
+    exit(2)
+
+trans_model = whisperx.load_model(environ.get("TRANSCRYBE_MODEL_SIZE"), "cpu", compute_type="int8", language="en")
 align_model, align_model_meta = whisperx.load_align_model(
-    language_code="en", device="cpu")
+    language_code="en", device="cpu"
+)
 dia_model = whisperx.DiarizationPipeline(
-    use_auth_token=environ.get("TRANSCRYBE_HF_TOKEN"), device="cpu")
+    use_auth_token=environ.get("TRANSCRYBE_HF_TOKEN"), device="cpu"
+)
 
 
 def transcribe(filename: str):
     audio = whisperx.load_audio(filename)
     result = trans_model.transcribe(audio, batch_size=8)
-    result = whisperx.align(result["segments"], align_model,
-                            align_model_meta, audio, "cpu", return_char_alignments=False)
+    result = whisperx.align(
+        result["segments"],
+        align_model,
+        align_model_meta,
+        audio,
+        "cpu",
+        return_char_alignments=False,
+    )
     dia_segments = dia_model(audio)
     result = whisperx.assign_word_speakers(dia_segments, result)
     return result
