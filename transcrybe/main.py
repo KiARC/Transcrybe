2a8e7719fe96fb63badb59d469638c8b37ecd755
Katherine Rose <kiarc@kiarc.win>
2024-09-13 13:31:52 -0400

    Remove logging, it will be moved to lib.py
    When logging happens in the route function it only gets printed when the function returns

--- transcrybe/main.py
+++ transcrybe/main.py
@@ -1,19 +1,14 @@
 import os
 from fastapi import FastAPI, UploadFile
 from transcrybe import lib
-import time
 
 app = FastAPI()
 
 
 @app.post("/transcribe")
 def process_route(audio: UploadFile):
-    print(f"Processing \"{audio.filename}\"...")
     with open(f"/tmp/{audio.filename}", "wb") as file:
         file.write(audio.file.read())
-    start = time.perf_counter()
     result = lib.transcribe(f"/tmp/{audio.filename}")
-    end = time.perf_counter()
-    print(f"Done processing \"{audio.filename}\" in {end-start}s")
     os.remove(f"/tmp/{audio.filename}")
     return {"filename": audio.filename, "transcript_segments": [{"text": s["text"], "speaker": s["speaker"], "start": s["start"], "end": s["end"]} for s in result["segments"]]}
